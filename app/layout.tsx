08e7d4412c05203f978d2286deb362fd08d36beb
djerbiano <saberghoudi2222@hotmail.fr>
2025-04-22 18:32:08 +0200

    add Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
